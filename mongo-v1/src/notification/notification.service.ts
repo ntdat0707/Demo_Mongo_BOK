17a1bbbdcb39b285896cdc450d52242635ae9f8a
dattan0707 <luckyrain07@gmail.com>
2020-07-22 14:47:47 +0700

    Done  - Email notification

--- mongo-v1/src/notification/notification.service.ts
+++ mongo-v1/src/notification/notification.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { BookingService } from 'src/booking/booking.service';
 import { Booking } from 'src/booking/booking.entity';
+//import { nodemailer } from 'nodemailer';
 
 @Injectable()
 export class NotificationService {
@@ -9,10 +10,10 @@ export class NotificationService {
   async getCustomerBooking(customer_id: number): Promise<Booking> {
     const bookings = await this.getBookings();
     let found = false;
-    for (let booking of bookings) {
-      if (booking.user['user_id'] === customer_id) {
+    for (const booking of bookings) {
+      if (booking.user['user_id'] == customer_id) {
         found = true;
-        return booking;
+        return await booking;
       }
     }
     if (found == false) {
@@ -20,9 +21,50 @@ export class NotificationService {
     }
   }
 
-  async sendEmailNotification() {}
+  async sendEmailNotification(content: string, id: number): Promise<void> {
+    //setting nodemailer
+    var nodemailer = require('nodemailer');
+    const booking = await this.createEmailContent(id);
+    var transpoter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'luckyrain07@gmail.com', //edit less security apps on gmail
+        pass: '091392134112',
+      },
+    });
+
+    //Setting email
+    var mailOptions = {
+      from: 'luckyrain07@gmail.com',
+      to: 'nguyentandat.email07@gmail.com',
+      subject: content,
+      text: `Hi ${booking.user['name']} 
+     This is your Booking detail:Booking Time: ${booking['cus_booking_time']} ,
+      Service:${booking.product['product_name']}, Dentist:${booking['dentist_name']}`,
+      html: `<p>Hi,<strong>${booking.user['name']}</strong><br>This is your Booking detail:<br>
+      -\nBooking Time:\n<strong>${booking['cus_booking_time']}</strong><br>-\nService:\n<strong>${booking.product['product_name']}</strong><br>-\nDentist:\n<strong>${booking['dentist_name']}</strong></p>`,
+    };
+
+      transpoter.sendMail(mailOptions, (error, infor) => {
+      if (error) {
+        console.log('Error mail', error);
+      } else {
+        console.log('Email sent' + JSON.stringify(infor));
+      }
+    });
+  }
 
   async getBookings(): Promise<Booking[]> {
     return this.bookingService.getBooking();
+  }
+
+  async createEmailContent(id: number): Promise<Booking> {
+    const bookings = await this.getBookings();
+    const booking = await bookings.find(booking => booking.booking_id == id);
+    if (!booking) {
+      throw new NotFoundException('Not found this booking');
+    } else {
+      return booking;
+    }
   }
 }
