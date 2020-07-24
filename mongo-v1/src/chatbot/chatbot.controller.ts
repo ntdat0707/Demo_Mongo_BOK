f8f9a231f7ad52175dc55f0c792f2a2fa532324f
dattan0707 <luckyrain07@gmail.com>
2020-07-24 18:05:46 +0700

    Fix axios

--- mongo-v1/src/chatbot/chatbot.controller.ts
+++ mongo-v1/src/chatbot/chatbot.controller.ts
@@ -1,48 +1,19 @@
 import {
   Controller,
   Post,
-  Body,
-  Get,
-  Query,
-  ValidationPipe,
 } from '@nestjs/common';
-import { ChatbotService } from './chatbot.service';
-import { CreateChatbotDTO } from './middleware/create-chatbot-dto';
-import { Chatbot } from './chatbot.entity';
-import { response } from 'express';
 
 @Controller('chatbot')
 export class ChatbotController {
-  constructor(private chatbotService: ChatbotService) {}
+  constructor() {}
 
-  // @Get('/rasa')
-  // async getReply(@Body(ValidationPipe)chatbotDTO: CreateChatbotDTO):Promise<Chatbot>{
-  //   return this.chatbotService.getReply(chatbotDTO);
-  // }
-
-  // @Post('/rasa')
-  // async getReply(@Body(ValidationPipe) chatbotDTO: CreateChatbotDTO):Promise<Chatbot> {
-  //   console.log("Reply",JSON.stringify(chatbotDTO));
-  //   return this.chatbotService.getReply(chatbotDTO);
-  // }
-
+  //Demo connect - get request --- response
   @Post()
   async getReply() {
-    const axios = require('axios');
+    const axios = require('axios').create({baseURL:'http://192.168.1.101:5005'});
     axios
       .post('webhooks/restnew/webhook', {
-        baseURL: 'http://192.168.1.101:5005',
-        body: {
-          message: 'hello',
-        },
-        proxy: {
-          host: '192.168.1.102',
-          port: 3000,
-        },
-        // proxy: {
-        //   host: '192.168.1.101',
-        //   port: 5005,
-        // },
+          message: 'bye'
       })
       .then(response => {
         console.log(response.data);
@@ -50,19 +21,6 @@ export class ChatbotController {
       .catch(error => {
         console.log(error);
       });
-    // axios
-    //   .get('/sp', {
-    //     baseURL: 'https://localhost',
-    //     // body: {
-    //     //   message: 'hello',
-    //     // },
-    //     proxy: {
-    //       host: '192.168.1.102',
-    //       port: 3000,
-    //     },
-    //   })
-    //   .then(function(response) {
-    //     console.log('OKE Data', response.data);
-    //   });
+
   }
 }
