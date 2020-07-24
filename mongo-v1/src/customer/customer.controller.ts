750dc33c00caf2c47539b0b6f42dd9b77eee6f6b
dattan0707 <luckyrain07@gmail.com>
2020-07-24 10:51:13 +0700

    Done - Get Customer infor when logged

--- mongo-v1/src/customer/customer.controller.ts
+++ mongo-v1/src/customer/customer.controller.ts
@@ -5,14 +5,18 @@ import {
   Body,
   Param,
   ValidationPipe,
-  Query,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDTO } from './middleware/create-customer-dto';
 import { Customer } from './customer.entity';
+import { JwtAuthGuard } from 'src/auth/middleware/jwt-auth.guard';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('customer')
+@UseGuards(JwtAuthGuard)
 export class CustomerController {
   constructor(private customerService: CustomerService) {}
 
@@ -29,7 +33,12 @@ export class CustomerController {
   }
 
   @Get('/:id')
-  async getUser(@Param('id',ParseIntPipe) id: number): Promise<Customer> {
+  async getUser(@Param('id', ParseIntPipe) id: number): Promise<Customer> {
     return this.customerService.getUser(id);
+  }
+
+  @Post('/logged')
+  async getUserLoggedInfo(@GetUser() user: User):Promise<Customer> {
+   return this.customerService.getUserLoggedInfo(user);
   }
 }
